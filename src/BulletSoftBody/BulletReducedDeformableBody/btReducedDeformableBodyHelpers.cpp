06225a4f1cd0d2604df5079d05d9e5f922174141
Soft eng <omg@hu.com>
2025-07-15 19:01:26 +0800

    done

--- src/BulletSoftBody/BulletReducedDeformableBody/btReducedDeformableBodyHelpers.cpp
+++ src/BulletSoftBody/BulletReducedDeformableBody/btReducedDeformableBodyHelpers.cpp
@@ -115,7 +115,7 @@ btReducedDeformableBody* btReducedDeformableBodyHelpers::createFromVtkFile(btSof
 	rsb->m_tetraScratches.resize(rsb->m_tetras.size());
 	rsb->m_tetraScratchesTn.resize(rsb->m_tetras.size());
 	printf("Nodes:  %u\r\n", rsb->m_nodes.size());
-    printf("Links:  %u\r\n", (unsigned int)rsb->m_links.size());
+	printf("Links:  %u\r\n", rsb->m_links.size());
 	printf("Faces:  %u\r\n", rsb->m_faces.size());
 	printf("Tetras: %u\r\n", rsb->m_tetras.size());
 
